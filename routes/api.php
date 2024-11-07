284ae45a4e15029bc9af78647295dfd778651207
John Paul <johnpaulustaris@gmail.com>
2024-11-08 07:07:48 +0800

    power

--- routes/api.php
+++ routes/api.php
@@ -90,6 +90,3 @@ Route::middleware(['auth:sanctum'])->group(function () {
 
     Route::post('login', [AuthController::class, 'login']);
     Route::post('logout', [AuthController::class, 'logout']);
-
-    Route::get('test', [ResidentController::class, 'test']);
-    
