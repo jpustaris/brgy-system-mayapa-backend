78983fbf6d32b90484798873c7a76fb1d700387a
John Paul Ustaris <johnpaulustaris@gmail.com>
2024-11-13 16:52:31 +0800

    fixed issues

--- config/sms.php
+++ config/sms.php
@@ -3,7 +3,7 @@
 return [
     // https://semaphore.co/
     'api_path' => "https://api.semaphore.co/api/v4/messages", 
-    'sms_api_key' => "dde2eaf087b3f14ff529aa07d3ea2608", 
-    'display_name' => "BrgyMayapa", 
+    'sms_api_key' => "", 
+    'display_name' => "test", 
 
 ];
